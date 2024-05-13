822680441a411d54b74ea687836fd760a0d6328a
Fardin Rahman <102713776+fardinGG@users.noreply.github.com>
2024-05-13 01:21:10 +0000

    Fix issue

--- spend-counter/src/App.js
+++ spend-counter/src/App.js
@@ -1,16 +1,27 @@
-// App.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ExpenseForm from './components/expenseForm';
 import ExpenseTable from './components/expenseTable'
 import MonthlyTotal from './components/monthlyTotal';
-import Button from 'react-bootstrap/Button';
+import { Button } from 'react-bootstrap'; 
+
 import * as XLSX from 'xlsx';
 
 function App() {
   const [expenses, setExpenses] = useState([]);
 
+  useEffect(() => {
+    const storedExpenses = JSON.parse(localStorage.getItem('expenses')) || [];
+    setExpenses(storedExpenses);
+  }, []);
+
   const addExpense = (expense) => {
-    setExpenses([...expenses, expense]);
+    const updatedExpenses = [...expenses, expense];
+    setExpenses(updatedExpenses);
+    localStorage.setItem('expenses', JSON.stringify(updatedExpenses));
+  };
+  const clearExpenses = () => {
+    setExpenses([]);
+    localStorage.removeItem('expenses');
   };
 
   const downloadExpenses = () => {
@@ -25,16 +36,12 @@ function App() {
       <h1>Spend Counter</h1>
       <ExpenseForm onAddExpense={addExpense} />
       <ExpenseTable expenses={expenses} />
-      <Button variant="secondary" onClick={downloadExpenses}>Download Expenses</Button>
+      <Button onClick={downloadExpenses}>Download Expenses</Button>
+      <Button variant="danger" onClick={clearExpenses}>Clear Expenses</Button>
       <MonthlyTotal expenses={expenses} />
       
     </div>
   );
 }
-
-
-
-
-
 
 export default App;
